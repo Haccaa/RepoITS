6b2513326ba81b1c6f60a704a2fba93ed2d16986
Haccaa <christian.ferretti.14-2@accademiadigitaleliguria.it>
2025-06-25 15:33:45 +0200

    cose

---
+++ progetto/ConsoleApp1/TestCalcolatrice/Test1.cs
@@ -0,0 +1,22 @@
+﻿namespace TestCalcolatrice
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSomma()
+        {
+            CalcolatriceLib.Calcolatrice calcolatrice = new CalcolatriceLib.Calcolatrice();
+            double risultato = calcolatrice.Somma(2, 3);
+            Assert.AreEqual(5, risultato, "La somma di 2 e 3 dovrebbe essere 5.");
+        }
+
+        [TestMethod]
+        public void TestSommaConZero()
+        {
+            CalcolatriceLib.Calcolatrice calcolatrice = new CalcolatriceLib.Calcolatrice();
+            double risultato = calcolatrice.Somma(0, 3);
+            Assert.AreEqual(3, risultato, "La somma di 0 e 3 dovrebbe essere 3");
+        }
+    }
+}
